4b186e4e9039625a941fc384b15b2e71a2f8a20a
PARVATHI PARU <parvathiparu1030@gmail.com>
2024-06-25 19:49:59 +0530

    First Push

---
+++ api/setting_master/setting_master.service.js
@@ -0,0 +1,64 @@
+const { pool } = require('../../config/config')
+module.exports = {
+    settingMasterInsert: (data, callback) => {
+        pool.query(
+            `INSERT INTO setting_master(
+             clinic_name, clinic_address, clinic_mobile, reg_renewaldays
+                )
+                VALUES(?,?,?,?)`,
+            [
+                data.clinic_name,
+                data.clinic_address,
+                data.clinic_mobile,
+                data.reg_renewaldays
+            ],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+                return callback(null, results);
+            }
+
+        );
+    },
+
+    settingMasterUpdate: (data, callback) => {
+        pool.query(
+            `UPDATE setting_master 
+            SET clinic_name=?,
+            clinic_address=?,
+            clinic_mobile=?,
+            reg_renewaldays=?
+            WHERE master_slno=? `,
+            [
+                data.clinic_name,
+                data.clinic_address,
+                data.clinic_mobile,
+                data.reg_renewaldays,
+                data.master_slno
+            ],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+                return callback(null, results);
+            }
+        );
+    },
+    SettingMasterGet: (callback) => {
+        pool.query(
+            `SELECT clinic_name, clinic_address, clinic_mobile, reg_renewaldays
+            FROM setting_master
+           `,
+            [],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+                return callback(null, results);
+            }
+        );
+    },
+
+
+}
